62f8057b82d440e6dfd5741323b5d70b3dd76fae
ElsaneHesenova <160469303+ElsaneHesenova@users.noreply.github.com>
2024-03-11 11:30:40 -0700

    Add files via upload

---
+++ dom java/script.js
@@ -0,0 +1,18 @@
+    let button1 = document.getElementById("button1");
+    let button2 = document.getElementById("button2");
+    let button3 = document.getElementById("button3");
+
+    let loremText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+
+    button1.addEventListener("click", function() {
+        alert("click Show modal 1");
+    });
+
+
+    button2.addEventListener("click", function() {
+        alert("click Show modal 2");
+    });
+
+    button3.addEventListener("click", function() {
+        alert("click Show modal 3");
+    });
